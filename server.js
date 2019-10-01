dc5299417ac6ae6478ad7b900231cccdc0ccf4ad
SteveObee <stephenobee1@gmail.com>
2019-10-01 13:56:11 +0100

    Pre deploy 1:56pm

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, 'build')));
+}
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
