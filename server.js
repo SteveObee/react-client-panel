4cb6c95771db843e420dc35d01f751e476e7b6b0
SteveObee <stephenobee1@gmail.com>
2019-10-01 14:04:44 +0100

    Pre deploy 2:04pm

--- server.js
+++ server.js
@@ -12,4 +12,6 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
